5d4944d7b26b8a91f3d44fe94a4eae6cdfa545ed
halith-smh <mohamedhalith.smh@gmail.com>
2024-03-28 23:09:21 +0530

    Server deploy config

--- server/server.js
+++ server/server.js
@@ -1,65 +1,70 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 
-const mongoose = require('mongoose');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
-const AuthRouter = require('./routes/Auth');
-const PostRouter = require('./routes/Posts');
-const AdminRouter = require('./routes/Admin');
+const AuthRouter = require("./routes/Auth");
+const PostRouter = require("./routes/Posts");
+const AdminRouter = require("./routes/Admin");
 
-const PublicRouter = require('./routes/Public');
+const PublicRouter = require("./routes/Public");
 
-const { verifyAdmin } = require('./middleware/Admin');
+const { verifyAdmin } = require("./middleware/Admin");
 
-const { verifyUser } = require('./middleware/User');
+const { verifyUser } = require("./middleware/User");
 
-
-const path = require('path');
+const path = require("path");
 const app = express();
 
 app.use(express.json());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
     methods: ["GET", "POST", "PATCH"],
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
+  console.log(req.path, req.method);
+  next();
+});
+
+//deploy preview - test
+app.get("/", (req, res) => {
+  res.send("<h1>IGNITE - 2k24</h1>");
 });
 
 // Public Img access directory
-const uploadsDirectory = path.join(__dirname, 'uploads');
-app.get('/uploads/:date/:filename', (req, res) => {
-    const { date, filename } = req.params;
-    const imagePath = path.join(uploadsDirectory, date, filename);
-    res.sendFile(imagePath);
+const uploadsDirectory = path.join(__dirname, "uploads");
+app.get("/uploads/:date/:filename", (req, res) => {
+  const { date, filename } = req.params;
+  const imagePath = path.join(uploadsDirectory, date, filename);
+  res.sendFile(imagePath);
 });
 
 //auth
-app.use('/api/auth', AuthRouter);
+app.use("/api/auth", AuthRouter);
 
 //post
-app.use('/api/posts', PostRouter);
+app.use("/api/posts", PostRouter);
 
 //admin
-app.use('/api/admin/', verifyAdmin ,AdminRouter);
+app.use("/api/admin/", verifyAdmin, AdminRouter);
 
 //public routes - auth common users routes
-app.use('/api/', verifyUser, PublicRouter);
+app.use("/api/", verifyUser, PublicRouter);
 
-
-
-
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     app.listen(process.env.PORT, () => {
-        console.log('Listen to port 4000');
+      console.log("Listen to port 4000");
     });
-}).catch((error) => {
+  })
+  .catch((error) => {
     console.log(error);
-})
+  });
